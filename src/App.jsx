abec07f8a0f284260c4c5bdfeae7abd96eb9ce36
Madhesh-Dev <medasanimadhesh@gmail.com>
2025-05-31 10:38:07 +0530

    add pages and line component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage/LandingPage";
 import DetailsPage from "./pages/DetailsPage/DetailsPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
     return (
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import classes from "./DetailsPage.module.css";
 
 function DetailsPage() {
-    return <div>DetailsPage</div>;
+    return (
+        <div>
+            <LineChart />
+        </div>
+    );
 }
 
 export default DetailsPage;
