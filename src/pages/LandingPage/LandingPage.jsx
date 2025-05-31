ea05a8c8ff419f158dd2feb3775d0c70b93c7335
Madhesh-Dev <medasanimadhesh@gmail.com>
2025-05-31 13:25:20 +0530

    move data inside

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import Map, { Source, Layer, Popup } from "react-map-gl";
 import React from "react";
 import classes from "./LandingPage.module.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { Typography } from "@mui/material";
 
 export default function LandingPage() {
     const [viewState, setViewState] = React.useState({
@@ -11,13 +12,23 @@ export default function LandingPage() {
     });
 
     return (
-        <div className={classes["map-container"]}>
-            <Map
-                reuseMaps={true}
-                mapStyle="mapbox://styles/mapbox/dark-v9"
-                mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
-                renderWorldCopies={false}
-            />
+        <div className={classes["landing-container"]}>
+            <div className={classes["userinfo-widgets-container"]}>
+                <Typography variant="h4" color="white" component="span">
+                    Hello User,
+                </Typography>
+
+                <div className={classes["widgets-container"]}></div>
+            </div>
+
+            <div className={classes["map-container"]}>
+                <Map
+                    reuseMaps={true}
+                    mapStyle="mapbox://styles/mapbox/dark-v9"
+                    mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+                    renderWorldCopies={false}
+                />
+            </div>
         </div>
     );
 }
