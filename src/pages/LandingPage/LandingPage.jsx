be030c36f277a1dec6bc1277db1ee3ebc419ae14
Madhesh-Dev <medasanimadhesh@gmail.com>
2025-05-31 12:38:34 +0530

    pushing

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,21 @@
+import Map, { Projection, Source, Layer, useMap, Popup } from "react-map-gl";
 import React from "react";
 import classes from "./LandingPage.module.css";
 
-function LandingPage() {
-    return <div>LandingPage</div>;
-}
+export default function LandingPage() {
+    const [viewState, setViewState] = React.useState({
+        longitude: -100,
+        latitude: 40,
+        zoom: 3.5,
+    });
 
-export default LandingPage;
+    return (
+        <div className={classes["map-container"]}>
+            <Map
+                mapStyle="mapbox://styles/mapbox/dark-v9"
+                mapboxApiAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+            />
+            Hello
+        </div>
+    );
+}
