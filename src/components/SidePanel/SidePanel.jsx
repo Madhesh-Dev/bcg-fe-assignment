16d93a1c63f4f7a5080d3068507d5a2ec5f0ee58
Madhesh-Dev <medasanimadhesh@gmail.com>
2025-06-01 15:42:29 +0530

    added hello user

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,140 +1,150 @@
-import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
-import { Tabs, Tab, Checkbox, Typography, IconButton } from '@mui/material';
-import { useState } from 'react';
-import { MailOutline } from '@mui/icons-material';
-import classes from './SidePanel.module.css';
-import inventoryMockData from '../../data/inventoryMockData.json';
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { Tabs, Tab, Checkbox, Typography, IconButton } from "@mui/material";
+import { useState } from "react";
+import { MailOutline } from "@mui/icons-material";
+import classes from "./SidePanel.module.css";
+import inventoryMockData from "../../data/inventoryMockData.json";
+import { useNavigate } from "react-router-dom";
 
 function StackCard({ details = {}, selectedCard, setSelectedCard }) {
-	const { cardName, cardId } = details;
+    const { cardName, cardId } = details;
 
-	return (
-		<div
-			onClick={() => setSelectedCard(cardId)}
-			style={{
-				padding: '16px 8px 16px 16px',
-				display: 'flex',
-				justifyContent: 'space-between',
-				alignItems: 'flex-start',
-				color: '#fff',
-				backgroundColor: selectedCard === cardId ? '#0D1B2A' : 'inherit',
-				border: selectedCard === cardId ? '1px solid #1976d2' : 'unset',
-				borderRadius: '4px',
-				cursor: 'pointer',
-			}}
-		>
-			<div style={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
-				<Checkbox
-					sx={{
-						color: '#ccc',
-						padding: 0,
-						width: '24px',
-						height: '24px',
-					}}
-					checked={selectedCard === cardId}
-				/>
-				<div>
-					<div
-						style={{
-							fontSize: '16px',
-							fontWeight: '500',
-							color: '#303030',
-							marginBottom: '8px',
-							background: '#fff',
-							padding: '2px 4px',
-							borderRadius: '4px',
-						}}
-					>
-						{cardName}
-					</div>
+    return (
+        <div
+            onClick={() => setSelectedCard(cardId)}
+            style={{
+                padding: "16px 8px 16px 16px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "flex-start",
+                color: "#fff",
+                backgroundColor:
+                    selectedCard === cardId ? "#0D1B2A" : "inherit",
+                border: selectedCard === cardId ? "1px solid #1976d2" : "unset",
+                borderRadius: "4px",
+                cursor: "pointer",
+            }}
+        >
+            <div style={{ display: "flex", flexDirection: "row", gap: "16px" }}>
+                <Checkbox
+                    sx={{
+                        color: "#ccc",
+                        padding: 0,
+                        width: "24px",
+                        height: "24px",
+                    }}
+                    checked={selectedCard === cardId}
+                />
+                <div>
+                    <div
+                        style={{
+                            fontSize: "16px",
+                            fontWeight: "500",
+                            color: "#303030",
+                            marginBottom: "8px",
+                            background: "#fff",
+                            padding: "2px 4px",
+                            borderRadius: "4px",
+                        }}
+                    >
+                        {cardName}
+                    </div>
 
-					<Typography variant="body2" color="white">
-						Sample Stack
-					</Typography>
-				</div>
-			</div>
-			<IconButton sx={{ color: '#27E1C1' }}>
-				<MailOutline fontSize="small" />
-			</IconButton>
-		</div>
-	);
+                    <Typography variant="body2" color="white">
+                        Sample Stack
+                    </Typography>
+                </div>
+            </div>
+            <IconButton sx={{ color: "#27E1C1" }}>
+                <MailOutline fontSize="small" />
+            </IconButton>
+        </div>
+    );
 }
 
 function SidePanel({
-	setIsSidebarOpen = () => {},
-	selectedCard,
-	setSelectedCard = () => {},
+    setIsSidebarOpen = () => {},
+    selectedCard,
+    setSelectedCard = () => {},
 }) {
-	const [tabIndex, setTabIndex] = useState(0);
+    const navigate = useNavigate();
 
-	const { cities = [] } = inventoryMockData;
+    const [tabIndex, setTabIndex] = useState(0);
 
-	const sampleData = cities[0].detailSets;
+    const { cities = [] } = inventoryMockData;
 
-	return (
-		<div className={classes.side_panel}>
-			<div
-				role="presentation"
-				className={classes.close_button}
-				style={{
-					top: '24px',
-					right: '0',
-					padding: '2px 4px',
-					borderRadius: '4px',
-					backgroundColor: '#27e1c1',
-					zIndex: '999',
-				}}
-				onClick={() => {
-					setIsSidebarOpen(false);
-				}}
-			>
-				<KeyboardDoubleArrowLeftIcon />
-			</div>
+    const sampleData = cities[0].detailSets;
 
-			<div className={classes.header}>
-				<KeyboardBackspaceIcon style={{ color: '#fff' }} />
+    return (
+        <div className={classes.side_panel}>
+            <div
+                role="presentation"
+                className={classes.close_button}
+                style={{
+                    top: "24px",
+                    right: "0",
+                    padding: "2px 4px",
+                    borderRadius: "4px",
+                    backgroundColor: "#27e1c1",
+                    zIndex: "999",
+                }}
+                onClick={() => {
+                    setIsSidebarOpen(false);
+                }}
+            >
+                <KeyboardDoubleArrowLeftIcon />
+            </div>
 
-				<div>Sample stack</div>
-			</div>
+            <div className={classes.header} onClick={() => navigate(-1)}>
+                <KeyboardBackspaceIcon style={{ color: "#fff" }} />
 
-			<Tabs
-				value={tabIndex}
-				onChange={(e, newValue) => setTabIndex(newValue)}
-				textColor="inherit"
-				TabIndicatorProps={{
-					sx: {
-						backgroundColor: '#27E1C1',
-						height: '3px',
-					},
-				}}
-			>
-				<Tab
-					label={`BACKLOG (${sampleData.length})`}
-					sx={{ color: tabIndex === 0 ? '#27E1C1' : '#888' }}
-				/>
-				<Tab
-					label="PENDING (0)"
-					sx={{ color: tabIndex === 1 ? '#27E1C1' : '#888' }}
-				/>
-				<Tab
-					label="FINAL SIGN-OFF (0)"
-					sx={{ color: tabIndex === 2 ? '#27E1C1' : '#888' }}
-				/>
-			</Tabs>
+                <div>Sample stack</div>
+            </div>
 
-			<div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-				{sampleData.map((item, index) => (
-					<StackCard
-						key={index}
-						details={item}
-						selectedCard={selectedCard}
-						setSelectedCard={setSelectedCard}
-					/>
-				))}
-			</div>
-		</div>
-	);
+            <Tabs
+                value={tabIndex}
+                onChange={(e, newValue) => setTabIndex(newValue)}
+                textColor="inherit"
+                TabIndicatorProps={{
+                    sx: {
+                        backgroundColor: "#27E1C1",
+                        height: "3px",
+                    },
+                }}
+            >
+                <Tab
+                    label={`BACKLOG (${sampleData.length})`}
+                    sx={{ color: tabIndex === 0 ? "#27E1C1" : "#888" }}
+                />
+                <Tab
+                    label="PENDING (0)"
+                    sx={{ color: tabIndex === 1 ? "#27E1C1" : "#888" }}
+                />
+                <Tab
+                    label="FINAL SIGN-OFF (0)"
+                    sx={{ color: tabIndex === 2 ? "#27E1C1" : "#888" }}
+                />
+            </Tabs>
+
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "16px",
+                }}
+            >
+                {sampleData.map((item, index) => (
+                    <StackCard
+                        key={index}
+                        details={item}
+                        selectedCard={selectedCard}
+                        setSelectedCard={setSelectedCard}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default SidePanel;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,15 +4,19 @@ import { Typography } from "@mui/material";
 import Map from "@/components/Map/Map";
 import CardsWidget from "@/components/CardsWidget/CardsWidget";
 import mockData from "@/data/inventoryMockData.json";
+import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
 
 export default function LandingPage() {
     const cities = mockData.cities;
     return (
         <div className={classes["landing-container"]}>
             <div className={classes["userinfo-widgets-container"]}>
-                <Typography variant="h4" color="white" component="span">
-                    Hello User,
-                </Typography>
+                <h1>Hello User,</h1>
+
+                <div className={classes["action-items"]}>
+                    <InfoOutlineIcon sx={{ fontSize: 14 }} />
+                    There are 2 active actions
+                </div>
             </div>
 
             <CardsWidget cards={cities} />
